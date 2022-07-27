04d1c0a17db8e8b8661bdb53c048ebb7354316e3
AshKuraine <lpc9720@hotmail.com>
2022-07-27 15:30:42 -0500

    cambios de ultima hora

--- src/app/app/pages/reservation/reservation-details/reservation-details.component.ts
+++ src/app/app/pages/reservation/reservation-details/reservation-details.component.ts
@@ -8,18 +8,19 @@ import { ReservationServices } from 'src/app/app/core/services/reservation.servi
 })
 export class ReservationDetailsComponent implements OnInit {
 
-  constructor(public reservationServices : ReservationServices) {
-    this.reservationServices.reservationSelected.email = "junior@gmail.com";
-    this.reservationServices.reservationSelected.id_room = "A5";
+  constructor(public reservationServices : ReservationServices,
+    ) {
+    this.reservationServices.reservationSelected.email = "rafaellopez@gmail.com";
+    this.reservationServices.reservationSelected.id_room = "C9";
     this.reservationServices.reservationSelected.number_adults = 2;
-    this.reservationServices.reservationSelected.number_children = 3;
-    this.reservationServices.reservationSelected.date_in = new Date("2022-06-15T00:00:00.000Z");
-    this.reservationServices.reservationSelected.date_out =new Date("2022-06-20T00:00:00.000Z");
-    this.reservationServices.reservationSelected.payment = 322;
+    this.reservationServices.reservationSelected.number_children = 2;
+    this.reservationServices.reservationSelected.date_in = new Date("2022-07-26T00:00:00.000Z");
+    this.reservationServices.reservationSelected.date_out =new Date("2022-07-30T00:00:00.000Z");
    }
 
   ngOnInit(): void {
     console.log(this.reservationServices.reservationSelected);
+    console.log(this.reservationServices.search);
   }
 
   reservar(){
